64b1d66e37d5b6586d0388f9c876aa94d9e6cf82
Jason Jorgensen <jasoncjorgensen@gmail.com>
2020-12-20 16:02:18 -0700

    Password generator function was updated and working and inserts randomized password in text box.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+
+
 //Character Types
-special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", ">", "?", "@", "[", "\\", "]","^", "_", "`", "{", "|", "}", "~"];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Write password to the #password input
 function writePassword() {
@@ -19,28 +24,59 @@ function writePassword() {
   var lowerCase = confirm("Would you like to include lowercase character types?");
   var upperCase = confirm("Would you like to include uppercase character types?");
   var numeric = confirm("Would you like to include numeric character types?");
-  var upperCase = confirm("Would you like to include spceical character types?");
-  if (lowerCase === false && upperCase === false && numeric === false && upperCase === false) {
+  var specialChar = confirm("Would you like to include spceical character types?");
+  if (lowerCase === false && upperCase === false && numeric === false && special === false) {
    alert("You must select at least one character type.")
   }
   } while ( 
-    lowerCase === false && upperCase === false && numeric === false && upperCase === false );
+    lowerCase === false && upperCase === false && numeric === false && upperCase === false);
   
+  //Creating a password character array
+  var passwordCharacters = []
+  
+  if (lowerCase) {
+    passwordCharacters = passwordCharacters.concat(letters);
+  }
 
-  
-  
+  if (upperCase) {
+    passwordCharacters = passwordCharacters.concat(alphaLetters);
+  }
+
+  if (numeric) {
+    passwordCharacters = passwordCharacters.concat(number);
+  }
+
+  if (specialChar) {
+    passwordCharacters = passwordCharacters.concat(special);
+  }
  
-  
+  //String that will contain the requested random password
+  var randomPassword = ""
 
-
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-
-  passwordText.value = password;
+  for (var i = 0; i < passwordLength; i++){
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)]; 
+  }
+  // console.log(passwordCharacters)
+  // console.log(randomPassword)
+  return randomPassword;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var password = writePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+
+// var password = generatePassword();
+// var password = writePassword();
+// var passwordText = document.querySelector("#password");
+// passwordText.value = password;
+
+
+
+
+
+
